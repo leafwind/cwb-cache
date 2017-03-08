4440001fd32c321a3bc3c0da1523a5ec8de69dd6
leafwind <leafwind.cs@gmail.com>
2017-03-08 05:28:24 +0000

    refactor

--- forecast_36hr.py
+++ forecast_36hr.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-import requests
 import json
 import logging
 import calendar
 import datetime
+
+import requests
+import sqlite3
 
 from cwb_auth_key import AUTH_KEY
 from constants import CWB_URL
@@ -12,20 +14,23 @@ from predict_code_map import PREDICT_CODE_MAP
 logging.basicConfig(level=logging.DEBUG)
 
 def get_data():
+    logging.info('getting data from CWB...')
     #'{dataid}?limit={limit}&offset={offset}&format={format}&locationName={locationName}&elementName={elementName}&sort={sort}'
-    
     r = requests.get(CWB_URL + '{}'.format('F-C0032-001'), headers={'Authorization': AUTH_KEY})
     
     if r.status_code != 200:
         logging.error('r.status_code: {}'.format(r.status_code))
-        return False
+        return None
+
     data = r.json()
     
     if data.get('success') != 'true':
-        return False
-    
+        return None
+    return data
+
+def parse_json_to_dict(data):
+    logging.info('parsing {} ...'.format(data['records']['datasetDescription'].encode('utf-8')))
     output = {}
-    logging.info('start process: {}'.format(data['records']['datasetDescription'].encode('utf-8')))
     locations = data['records']['location']
     for l in locations:
         location_name = l['locationName']
@@ -33,7 +38,6 @@ def get_data():
         factors = l['weatherElement']
         for f in factors:
             factor_name = f['elementName']
-            # output[location_name][factor_name] = []
             periods = f['time']
             for p in periods:
                 start_time_ts = calendar.timegm(datetime.datetime.strptime(p['startTime'], '%Y-%m-%d %H:%M:%S').timetuple()) - 8 * 3600
@@ -50,9 +54,14 @@ def get_data():
                     output[location_name][time_key][factor_name] = forecast_status
                 elif factor_name == 'CI':
                     output[location_name][time_key][factor_name] = forecast_status
+    return output
 
-    with open('output.json', 'w') as fp:
-        json.dump(output, fp)
+def dump_dict_to_json_file(dict_data, filename):
+    logging.info('dump to json file...')
+    with open(filename, 'w') as fp:
+        json.dump(dict_data, fp)
 
 if __name__ == '__main__':
-    get_data()
+    json_data = get_data()
+    dict_data = parse_json_to_dict(json_data)
+    dump_dict_to_json_file(dict_data, 'output.json')
